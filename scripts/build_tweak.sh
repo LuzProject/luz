0cf6785593e52e9f4be1938772a0b05dadd3bf34
jaidan <ajaidan0@gmail.com>
2023-01-24 16:15:39 -0500

    chore [SCRIPTS]: add scripts for compilation and install (for testing)

---
+++ scripts/build_tweak.sh
@@ -0,0 +1,11 @@
+MY_PATH="$(dirname -- "${BASH_SOURCE[0]}")"            # relative
+MY_PATH="$(cd -- "$MY_PATH" && pwd)"    # absolutized and normalized
+if [[ -z "$MY_PATH" ]] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+
+bash $MY_PATH/install.sh
+cd $MY_PATH/../tweak
+luz build
