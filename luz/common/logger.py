d878ba7d72a7096347085397678aa40e5bfca683
jaidan <ajaidan0@gmail.com>
2023-03-15 18:09:55 -0400

    fix (LOGGING): remove useless colors

--- luz/common/logger.py
+++ luz/common/logger.py
@@ -11,28 +11,13 @@ if name == "nt":
 
 
 colors = {
-    "black": "\033[30m",
     "red": "\033[31m",
     "green": "\033[32m",
     "orange": "\033[33m",
-    "blue": "\033[34m",
-    "purple": "\033[35m",
-    "cyan": "\033[36m",
-    "lightgrey": "\033[37m",
     "darkgrey": "\033[90m",
-    "lightred": "\033[91m",
-    "lightgreen": "\033[92m",
     "yellow": "\033[93m",
-    "lightblue": "\033[94m",
-    "pink": "\033[95m",
-    "lightcyan": "\033[96m",
     "reset": "\033[0m",
     "bold": "\033[01m",
-    "disable": "\033[02m",
-    "underline": "\033[04m",
-    "reverse": "\033[07m",
-    "strikethrough": "\033[09m",
-    "invisible": "\033[08m",
 }
 
 
